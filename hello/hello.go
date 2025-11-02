c9ccdb1775912cb7cfd5a24774ca8bb71764a234
waiwaicat <1853657658@qq.com>
2025-11-02 23:09:33 +0800

    log Fatal exit 1

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"example.com/greetings"
+	"fmt"
 	"log"
 )
 
 func main() {
 	log.SetPrefix("greetings: ")
 	//log.SetFlags(0) //不显示时间
-	log.Println("Hello")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 
 }
