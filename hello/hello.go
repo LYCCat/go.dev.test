b6bc9fc29b4cb48ddb87da50c2df695d4cb45496
waiwaicat <1853657658@qq.com>
2025-11-02 23:20:56 +0800

    random format

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,7 @@ func main() {
 	//log.SetFlags(0) //不显示时间
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Gladys")
 
 	if err != nil {
 		log.Fatal(err)
